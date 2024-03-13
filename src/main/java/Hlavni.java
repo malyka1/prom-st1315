381b5dddeb8e3281f650227b25d855aab6a0e5cb
Karel Malý <karel.maly@uhk.cz>
2024-03-13 14:21:35 +0100

    Doplnění dokumentace k třídám.

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -1,3 +1,6 @@
+/**
+ * Hlavní třída projektu.
+ */
 public class Hlavni {
     public static void main(String[] args) {
         System.out.println("Hlavní třída.");
--- src/main/java/Trida.java
+++ src/main/java/Trida.java
@@ -1,3 +1,6 @@
+/**
+ * Třída Trida.
+ */
 public class Trida {
     public static void main(String[] args) {
         System.out.println("Třída Trida.");
