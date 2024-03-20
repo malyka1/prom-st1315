19dd788291e7728e48d95857f56bb97de8b81e88
Karel Malý <karel.maly@uhk.cz>
2024-03-20 13:39:49 +0100

    Rozšíření třídy Hlavni

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -1,8 +1,10 @@
 /**
- * Hlavní třída projektu.
+ * Hlavní třída velkého projektu.
  */
 public class Hlavni {
     public static void main(String[] args) {
         System.out.println("Hlavní třída.");
+        System.out.println("Přidáno v klonu.");
     }
+
 }
